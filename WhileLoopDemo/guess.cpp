f31d400b5079b8db46801165683b3272120d8826
DarthSigil <servantofchristvigil@mail.com>
2022-05-08 04:09:02 -0400

    found & fixed err in guess.cpp -> wrong name for counter var

--- WhileLoopDemo/guess.cpp
+++ WhileLoopDemo/guess.cpp
@@ -7,24 +7,19 @@ int main() {
   int tries = 0;
 
   // Write a while loop here:
-  while (tries < 50) {
+  while (tries < 5) {
     if (guess == 8) {
       std::cout << "You guessed it! The number is 8! You win! \n";
       break;
     }
     std::cout << "I have a number 1-10.\n";
-    std::cout << "Please guess it: ";
+    std::cout << "Please guess it: \n";
     std::cin >> guess;
-    count += 1;
+    tries += 1;
   }
-  
-  
-  
-  
-  if (guess == 8) {
-    
-    std::cout << "You got it!\n";
-  
-  }  
+
+  if (tries >= 5) {
+    std::cout << "Out of guesses! You lose! \n";
+  }
   
 }
