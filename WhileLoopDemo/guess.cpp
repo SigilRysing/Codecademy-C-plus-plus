78fb9c5918853ea639e6ffa9a947f2eef8da7a9a
DarthSigil <servantofchristvigil@mail.com>
2022-05-08 04:40:29 -0400

    fiddled with guess.cpp a bit

--- WhileLoopDemo/guess.cpp
+++ WhileLoopDemo/guess.cpp
@@ -1,25 +1,34 @@
 #include <iostream>
-// C++ equiv of Python type(var)?
+
 int main() {
 
   int guess;
   
   int tries = 0;
 
+  
+ 
   // Write a while loop here:
-  while (tries < 5) {
-    if (guess == 8) {
-      std::cout << "You guessed it! The number is 8! You win! \n";
-      break;
-    }
+  while (tries < 3) {
+
     std::cout << "I have a number 1-10.\n";
-    std::cout << "Please guess it: \n";
+    std::cout << "Please guess it: ";
+
     std::cin >> guess;
+    
     tries += 1;
+
+    if (guess == 8) {
+    
+      std::cout << "You got it! 8 was my number! You win! \n";
+      break;
+  
+    }  
   }
 
-  if (tries >= 5) {
-    std::cout << "Out of guesses! You lose! \n";
+  if (guess != 8) {
+    std::cout << "You ran out of guesses! Game over! \n";
   }
+
   
 }
