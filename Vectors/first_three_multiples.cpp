79d5e4514717c53ec26c479875ed0888c814d1da
DarthSigil <servantofchristvigil@mail.com>
2022-05-16 02:55:10 -0400

    progress

---
+++ Vectors/first_three_multiples.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+
+// write a function that returns a vector of
+// the first three multiples of num is ascending order
+// Define first_three_multiples() here:
+
+
+int main() {
+  
+  for (int element : first_three_multiples(8)) {
+    std::cout << element << "\n";
+  }
+  
+}
